29db12dcf810daa8613c1eddf193ac312936f4e1
margoshiiik <81850799+margoshiiik@users.noreply.github.com>
2021-04-07 21:32:23 +0300

    Update Graphics.java

--- Program/Graphics.java
+++ Program/Graphics.java
@@ -1,8 +1,12 @@
 
+package Program;
+
 import javax.swing.*;
+import javax.swing.plaf.basic.BasicSplitPaneUI;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.nio.file.attribute.GroupPrincipal;
 import java.util.ArrayList;
 
 public class Graphics extends JFrame {
@@ -18,7 +22,7 @@ public class Graphics extends JFrame {
     private ArrayList<Group> groups;
     private ArrayList<Product> allProducts;
 
-    Graphics(Store store) {
+    public Graphics(Store store) {
         super("Store");
         this.myStore = store;
         this.groups = myStore.getGroups();
@@ -43,6 +47,7 @@ public class Graphics extends JFrame {
 
         JTextArea searchText = new JTextArea(1,20);
         searchText.setFont(new Font(Font.SERIF,Font.PLAIN,25));
+        String searchGroup = searchText.getText();
 
         JLabel store = new JLabel("Store");
         store.setForeground(new Color(47, 46, 50));
@@ -268,11 +273,15 @@ public class Graphics extends JFrame {
         JScrollPane scroll = new JScrollPane(productsList);
 
         String s = "";
-        for(int i = 0; i < oneGroupProducts.size()-1; i++)
-            s += oneGroupProducts.get(i).getName() + "\n";
-        s += oneGroupProducts.get(oneGroupProducts.size()-1).getName();
+        if (oneGroupProducts!=null) {
+            for (int i = 0; i < oneGroupProducts.size(); i++) {
+                s += oneGroupProducts.get(i).getName() + "\n";
+               // s += oneGroupProducts.get(oneGroupProducts.size() - 1).getName();
+            }
+            productsList.setText(s);
+        }
+        else productsList.setText("Товарів нема");
 
-        productsList.setText(s);
 
         JPanel southPanel = new JPanel(new FlowLayout());
         southPanel.setBackground(new Color(198, 233, 243));
@@ -313,15 +322,18 @@ public class Graphics extends JFrame {
             }
         });
 
-        JButton deleteCategory = new JButton("Видалити"); //Поки нічого не робить, але має видаляти групу
+
+        JButton deleteCategory = new JButton("Видалити"); //Видаляє групу і повертається на головну панель :)
         deleteCategory.setPreferredSize(new Dimension(120,25));
         deleteCategory.setBackground(new Color(128, 118, 146));
         deleteCategory.setForeground(new Color(255, 253, 253));
         deleteCategory.setFont(new Font(Font.SERIF,Font.PLAIN,17));
         deleteCategory.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e) {
 
+            public void actionPerformed(ActionEvent e) {
+                myStore.deleteGroup(groups.get(indexOfGroup).getName());
+                goToTheMainPanel();
             }
         });
 
@@ -356,6 +368,10 @@ public class Graphics extends JFrame {
         title.setFont(new Font(Font.SERIF,Font.PLAIN,30));
         titlePanel.add(title);
 
+        Product p = groups.get(indexOfGroup).getProducts().get(indexOfProduct);
+
+        JLabel product = new JLabel (p.toString());
+        productPanel.add(product);
 
         JPanel southPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
         southPanel.setBackground(new Color(198, 233, 243));
@@ -375,7 +391,9 @@ public class Graphics extends JFrame {
             }
         });
 
-        JButton deleteProduct = new JButton("Видалити"); //Поки нічого не робить, але має видаляти продукт
+        Group g = groups.get(indexOfGroup);
+
+        JButton deleteProduct = new JButton("Видалити"); //Видаляє продукт і повертається на сторінку з іншими продуктами :)
         deleteProduct.setPreferredSize(new Dimension(120,25));
         deleteProduct.setBackground(new Color(128, 118, 146));
         deleteProduct.setForeground(new Color(255, 253, 253));
@@ -383,7 +401,8 @@ public class Graphics extends JFrame {
         deleteProduct.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                g.deleteProduct(g.products.get(indexOfProduct).getName());
+                goToTheGroupPanel(indexOfGroup);
             }
         });
 
@@ -420,22 +439,94 @@ public class Graphics extends JFrame {
         pack();
     }
 
+    //створює вертикальний лейаут в вікнах для "додати категорію" і "додати продукт" :)
+    class VerticalLayout implements LayoutManager
+    {
+        private Dimension size = new Dimension();
+
+        // Следующие два метода не используются
+        public void addLayoutComponent   (String name, Component comp) {}
+        public void removeLayoutComponent(Component comp) {}
+
+        // Метод определения минимального размера для контейнера
+        public Dimension minimumLayoutSize(Container c) {
+            return calculateBestSize(c);
+        }
+        // Метод определения предпочтительного размера для контейнера
+        public Dimension preferredLayoutSize(Container c) {
+            return calculateBestSize(c);
+        }
+        // Метод расположения компонентов в контейнере
+        public void layoutContainer(Container container)
+        {
+            // Список компонентов
+            Component list[] = container.getComponents();
+            int currentY = 5;
+            for (int i = 0; i < list.length; i++) {
+                // Определение предпочтительного размера компонента
+                Dimension pref = list[i].getPreferredSize();
+                // Размещение компонента на экране
+                list[i].setBounds(5, currentY, pref.width, pref.height);
+                // Учитываем промежуток в 5 пикселов
+                currentY += 5;
+                // Смещаем вертикальную позицию компонента
+                currentY += pref.height;
+            }
+        }
+        // Метод вычисления оптимального размера контейнера
+        private Dimension calculateBestSize(Container c)
+        {
+            // Вычисление длины контейнера
+            Component[] list = c.getComponents();
+            int maxWidth = 0;
+            for (int i = 0; i < list.length; i++) {
+                int width = list[i].getWidth();
+                // Поиск компонента с максимальной длиной
+                if ( width > maxWidth )
+                    maxWidth = width;
+            }
+            // Размер контейнера в длину с учетом левого отступа
+            size.width = maxWidth + 5;
+            // Вычисление высоты контейнера
+            int height = 0;
+            for (int i = 0; i < list.length; i++) {
+                height += 5;
+                height += list[i].getHeight();
+            }
+            size.height = height;
+            return size;
+        }
+    }
+
+
+
     private void initAddCategoryPanel() {
-        addCategoryPanel = new JPanel(new BorderLayout());
+        addCategoryPanel = new JPanel(new VerticalLayout());
         addCategoryPanel.setBackground(new Color(198, 233, 243));
 
-        JPanel northPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        northPanel.setBackground(new Color(198, 233, 243));
+   //     JPanel northPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+    //    northPanel.setBackground(new Color(49, 138, 163));
 
         JLabel addCategory = new JLabel("Додати категорію");
         addCategory.setForeground(new Color(47, 46, 50));
         addCategory.setFont(new Font(Font.SERIF,Font.PLAIN,30));
 
-        JPanel southPanel = new JPanel((new FlowLayout(FlowLayout.LEFT)));
-        southPanel.setBackground(new Color(198, 233, 243));
+        JLabel newC = new JLabel("Введіть назву категорії");
+        newC.setFont(new Font(Font.SERIF,Font.PLAIN,20));
+        JLabel newD = new JLabel("Введіть опис категорії");
+        newD.setFont(new Font(Font.SERIF,Font.PLAIN,20));
+
+        JTextArea newCat = new JTextArea(2, 20);
+
+        JTextArea newDesc = new JTextArea(4, 20);
+
+
+
+    //    JPanel southPanel = new JPanel((new FlowLayout(FlowLayout.LEFT)));
+    //    southPanel.setBackground(new Color(0, 233, 243));
 
         JButton returnBack = new JButton("Назад");
-        returnBack.setPreferredSize(new Dimension(120,25));
+      //  returnBack.setPreferredSize(new Dimension(120,25));
         returnBack.setBackground(new Color(128, 118, 146));
         returnBack.setForeground(new Color(255, 253, 253));
         returnBack.setFont(new Font(Font.SERIF,Font.PLAIN,17));
@@ -446,12 +537,44 @@ public class Graphics extends JFrame {
             }
         });
 
-        northPanel.add(addCategory);
+        JButton addNewCategory = new JButton("Додати"); //Додає категорію до загального списку категорій, якщо вказане
+        // ім'я категорії, інакше повертається на початкову сторінку
+        addNewCategory.setBackground(new Color(128, 118, 146));
+        addNewCategory.setForeground(new Color(255, 253, 253));
+        addNewCategory.setFont(new Font(Font.SERIF,Font.PLAIN,17));
+        addNewCategory.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String name = newCat.getText();
+                String description = newDesc.getText();
+                if (!name.equals("")) {
+                    Group newGroup = new Group(name);
+                    if(description.equals("")){
+                        newGroup.setDescription("Опис відсутній");
+                    }
+                    else newGroup.setDescription(description);
+                    myStore.addGroup(newGroup);
+                }
+                goToTheMainPanel();
+            }
+        });
 
-        southPanel.add(returnBack);
 
-        addCategoryPanel.add(northPanel, "North");
-        addCategoryPanel.add(southPanel, "South");
+   //     addCategoryPanel.add(northPanel, "North");
+    //    addCategoryPanel.add(southPanel, "South");
+
+    //    northPanel.add(addCategory);
+        addCategoryPanel.add(addCategory);
+        addCategoryPanel.add(newC);
+        addCategoryPanel.add(newCat);
+        addCategoryPanel.add(newD);
+        addCategoryPanel.add(newDesc);
+        addCategoryPanel.add(returnBack);
+        addCategoryPanel.add(addNewCategory);
+
+       // southPanel.add(returnBack);
+
+
     }
 
     private void addProduct(int indexOfGroup) {
@@ -463,18 +586,83 @@ public class Graphics extends JFrame {
     }
 
     private void initAddProductPanel(int indexOfGroup) {
-        addProductPanel = new JPanel(new BorderLayout());
+        addProductPanel = new JPanel(new VerticalLayout());
         addProductPanel.setBackground(new Color(198, 233, 243));
 
-        JPanel northPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        northPanel.setBackground(new Color(198, 233, 243));
+    //    JPanel northPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+    //    northPanel.setBackground(new Color(198, 233, 243));
 
         JLabel addProduct = new JLabel("Додати продукт");
         addProduct.setForeground(new Color(47, 46, 50));
         addProduct.setFont(new Font(Font.SERIF,Font.PLAIN,30));
+        addProductPanel.add(addProduct);
 
-        JPanel southPanel = new JPanel((new FlowLayout(FlowLayout.LEFT)));
-        southPanel.setBackground(new Color(198, 233, 243));
+        JLabel newP = new JLabel("Введіть назву товару");
+        newP.setFont(new Font(Font.SERIF,Font.PLAIN,20));
+        addProductPanel.add(newP);
+        JTextArea enterP = new JTextArea(2, 20);
+        addProductPanel.add(enterP);
+
+        JLabel newPr = new JLabel("Введіть ціну");
+        newPr.setFont(new Font(Font.SERIF,Font.PLAIN,20));
+        addProductPanel.add(newPr);
+        JTextArea enterPr = new JTextArea(2, 20);
+        addProductPanel.add(enterPr);
+
+        JLabel newNumber = new JLabel("Введіть кількість товару на складі");
+        newNumber.setFont(new Font(Font.SERIF,Font.PLAIN,20));
+        addProductPanel.add(newNumber);
+        JTextArea enterNumber = new JTextArea(2, 20);
+        addProductPanel.add(enterNumber);
+
+        JLabel newBr = new JLabel("Введіть назву бренду");
+        newBr.setFont(new Font(Font.SERIF,Font.PLAIN,20));
+        addProductPanel.add(newBr);
+        JTextArea enterBr = new JTextArea(2, 20);
+        addProductPanel.add(enterBr);
+
+        JLabel newD = new JLabel("Введіть опис товару");
+        newD.setFont(new Font(Font.SERIF,Font.PLAIN,20));
+        addProductPanel.add(newD);
+        JTextArea enterD = new JTextArea(2, 20);
+        addProductPanel.add(enterD);
+
+
+        JButton addNewProduct = new JButton("Додати"); //додає продукт до загального списку продуктів, якщо введена
+        // назва товару, інакше повертається на сторінку групи :)
+        addNewProduct.setBackground(new Color(128, 118, 146));
+        addNewProduct.setForeground(new Color(255, 253, 253));
+        addNewProduct.setFont(new Font(Font.SERIF,Font.PLAIN,17));
+
+        addNewProduct.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String name = enterP.getText();
+                String price = enterPr.getText();
+                String newNumber = enterNumber.getText();
+                String brand = enterBr.getText();
+                String description = enterD.getText();
+                if (!name.equals("")) {
+                    Product pr = new Product(name);
+                    if(price.equals("")) pr.setPrice(0); else pr.setPrice(Integer.valueOf(price));
+                    if(newNumber.equals("")) pr.setNumber(0); else pr.setNumber(Integer.valueOf(newNumber));
+                    if(brand.equals("")) pr.setBrand("Не вказано"); else pr.setBrand(brand);
+                    if(description.equals("")){
+                        pr.setDescription("Опис відсутній");
+                    }
+                    else pr.setDescription(description);
+
+                    if(groups.get(indexOfGroup).getProducts().size() == 0)
+                        groups.get(indexOfGroup).getProducts().add(0, pr);
+
+                    else groups.get(indexOfGroup).getProducts().add(groups.get(indexOfGroup).getProducts().size(), pr);
+                }
+                goToTheGroupPanel(indexOfGroup);
+            }
+        });
+
+    //    JPanel southPanel = new JPanel((new FlowLayout(FlowLayout.LEFT)));
+    //    southPanel.setBackground(new Color(198, 233, 243));
 
         JButton returnBack = new JButton("Назад");
         returnBack.setPreferredSize(new Dimension(120,25));
@@ -488,12 +676,11 @@ public class Graphics extends JFrame {
             }
         });
 
-        northPanel.add(addProduct);
+        addProductPanel.add(addNewProduct);
+        addProductPanel.add(returnBack);
 
-        southPanel.add(returnBack);
-
-        addProductPanel.add(northPanel, "North");
-        addProductPanel.add(southPanel, "South");
+    //    addProductPanel.add(northPanel, "North");
+    //    addProductPanel.add(southPanel, "South");
     }
 
     private void goToTheMainPanel() {
