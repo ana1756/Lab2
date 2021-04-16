ccc9ef6ce35ba531efffdaa38c7695367f1e7d81
margoshiiik <81850799+margoshiiik@users.noreply.github.com>
2021-04-16 14:06:58 +0300

    Update Statistics.java

--- Program/Statistics.java
+++ Program/Statistics.java
@@ -5,6 +5,60 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class Statistics {
+    
+    public static void showStatistics(Store store) {
+
+        JTextArea text = new JTextArea();
+        text.setCaretPosition(0);
+        text.setEditable(false);
+        text.setFont(new Font(Font.SERIF, Font.PLAIN, 20));
+        text.setBackground(new Color(255, 255, 255));
+
+        String s = "Склад\n";  //всі товари
+        long sumMoney = 0; //сума грошей
+        double q = 0; //кількість одиниць товару
+        for (int i = 0; i < store.getGroups().size(); i++) {
+            if (store.getGroups().get(i).getProducts().size() != 0) {
+                s = s + "\n\n";
+                s = s + store.getGroups().get(i).getName().toUpperCase() + "\n";
+                for (int d = 0; d < store.getGroups().get(i).getProducts().size(); d++) {
+                    s = s + "\n" + store.getGroups().get(i).getProducts().get(d).getName().toUpperCase() + "\n";
+                    s = s + "Ціна: " + store.getGroups().get(i).getProducts().get(d).getPrice() + "\n";
+                    s = s + "Кількість " + Math.round(store.getGroups().get(i).getProducts().get(d).getNumber()) + "\n";
+                    s = s + "Виробник: " + store.getGroups().get(i).getProducts().get(d).getBrand() + "\n";
+                    s = s + "Опис: " + store.getGroups().get(i).getProducts().get(d).getDescription() + "\n";
+                }
+
+
+            } else {
+                s = s + "\n"+store.getGroups().get(i).getName().toUpperCase() + "\n ця група пуста" + "\n\n";
+            }
+        }
+
+        s = s + "\nВ магазині товари на суму: " + sumMoney + " гривень\n";
+        s = s + "Кількість одиниць товару: " + q;
+
+        text.setText(s);
+
+
+
+        JScrollPane jp = new JScrollPane(text);
+        jp.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        jp.setPreferredSize(new Dimension(450, 450));
+
+        JFrame frame = new JFrame();
+        frame.setSize(600, 500);
+        frame.setVisible(true); frame.setLocationRelativeTo(null);
+
+        JPanel panel =new JPanel();
+        panel.setPreferredSize(new Dimension(450, 450));
+        panel.setBackground(new Color(128, 118, 146));
+        panel.add(jp);
+
+        frame.add(panel);
+
+    }
+    
 
     //повертає, скільки на яку суму грошей міститься товару в групі
     public static long calculateMoney(Group group){
